9f1501047640cf3cd4f74248cc33a3394d03c7f2
Joe Moss <joemoss15@outlook.com>
2025-06-03 14:24:37 +0100

    Parameterise daily_min function

--- tests/test_models.py
+++ tests/test_models.py
@@ -30,14 +30,16 @@ def test_daily_mean_integers():
     npt.assert_array_equal(daily_mean(test_input), test_result)
 
 @pytest.mark.parametrize("test, expected", [
-                                            ([[1,2],[5,9],[4,4],[8,2]], [8,9])
+                                            ([[1,2],[5,9],[4,4],[8,2]], [8,9]),
+                                            ([[12,2],[5,9],[4,4],[8,2]], [12,9])
                                               ])
 def test_daily_max(test, expected):
     """Test that max function works for an array of positive integers."""
     npt.assert_array_equal(daily_max(test), expected)
 
 @pytest.mark.parametrize("test, expected", [
-                                            ([[1,2],[5,9],[4,4],[8,2]], [1,2])
+                                            ([[1,2],[5,9],[4,4],[8,2]], [1,2]),
+                                            ([[1,6],[2,7],[4,9],[1,4]], [1,4])
                                             ])
 def test_daily_min(test, expected):
     """Test that daily min function works for an array of postive integers."""
