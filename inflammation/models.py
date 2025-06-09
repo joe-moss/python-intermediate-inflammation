5f681d759e417df8e41c86254f473f05a922f7b7
Joe Moss <joemoss15@outlook.com>
2025-06-09 13:21:54 +0100

    Update normalise data function and test

--- inflammation/models.py
+++ inflammation/models.py
@@ -49,6 +49,16 @@ def daily_min(data):
 
 
 def patient_normalise(data):
-    """Normalise patient data from a 2D inflammation data array."""
-    patient_max_inflammation = np.max(data, axis=0)
-    return data / patient_max_inflammation[:, np.newaxis]
+    """
+    Normalise patient data from a 2D inflammation data array.
+
+    NaN values are ignored, and normalised to 0.
+
+    Negative values are rounded to 0.
+    """
+    max = np.nanmax(data, axis=1)
+    with np.errstate(invalid='ignore', divide='ignore'):
+        normalised = data / max[:, np.newaxis]
+    normalised[np.isnan(normalised)] = 0
+    normalised[normalised < 0] = 0
+    return normalised
