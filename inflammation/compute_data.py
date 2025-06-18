1e26b4b0a0118b828d58274a2d821f7113a16a6a
Joe Moss <joemoss15@outlook.com>
2025-06-18 13:40:42 +0100

    Add functions and tests

--- inflammation/compute_data.py
+++ inflammation/compute_data.py
@@ -16,41 +16,30 @@ class CSVDataSource:
         '''Loads inflammation data from CSV files within a specified directory'''
         data_file_paths = glob.glob(os.path.join(self.data_dir, 'inflammation*.csv'))
         if len(data_file_paths) == 0:
-            raise ValueError(f"No inflammation data CSV files found in path {self.data_dir}")    
+            raise ValueError(f"No inflammation data CSV files found in path {self.data_dir}")
         data = map(models.load_csv, data_file_paths)
         return list(data)
-
-class JSONDataSource:
-    '''Loads patient data with inflammation values from JSOn files within a specified directory'''
-    def __init__(self, data_dir):
-        self.data_dir = data_dir
-
-    def load_inflammation_data(self):
-        '''Loads inflammation data from JSON files within a specified directory'''
-        data_file_paths = glob.glob(os.path.join(self.data_dir, 'inflammation*.json'))
-        if len(data_file_paths) == 0:
-            raise ValueError(f"No inflammation JSON files found in path {self.data_dir}")    
-        data = map(models.load_json, data_file_paths)
-        return list(data)
-
-def analyse_data(data_source):
+    
+def analyse_data(data_dir):
     """Calculates the standard deviation by day between datasets.
-
-    Gets all the inflammation data from CSV files within a directory,
-    works out the mean inflammation value for each day across all datasets,
-    then plots the graphs of standard deviation of these means."""
-    data = data_source.load_inflammation_data()
-
-    means_by_day = map(models.daily_mean, data)
-    means_by_day_matrix = np.stack(list(means_by_day))
-
-    daily_standard_deviation = np.std(means_by_day_matrix, axis=0)
+    Gets all the inflammation data from CSV files within a directory, works out the mean
+    inflammation value for each day across all datasets, then visualises the
+    standard deviation of these means on a graph."""
+    data_file_paths = glob.glob(os.path.join(data_dir, 'inflammation*.csv'))
+    if len(data_file_paths) == 0:
+        raise ValueError(f"No inflammation csv's found in path {data_dir}")
+    data = map(models.load_csv, data_file_paths)
+    daily_standard_deviation = compute_standard_deviation_by_day(data)
 
     graph_data = {
         'standard deviation by day': daily_standard_deviation,
     }
     views.visualize(graph_data)
+    return daily_standard_deviation
 
+def compute_standard_deviation_by_day(data):
+    means_by_day = map(models.daily_mean, data)
+    means_by_day_matrix = np.stack(list(means_by_day))
 
-data_source1 = CSVDataSource(os.path.dirname('data/inflammation.csv'))
-analyse_data(data_source1)
+    daily_standard_deviation = np.std(means_by_day_matrix, axis=0)
+    return daily_standard_deviation
--- tests/test_models.py
+++ tests/test_models.py
@@ -1,14 +1,15 @@
 """Tests for statistics functions within the Model layer."""
 
+import math
 import numpy as np
 import numpy.testing as npt
+import pytest
+
 
 from inflammation.models import daily_mean
 
 def test_daily_mean_zeros():
     """Test that mean function works for an array of zeros."""
-    
-
     test_input = np.array([[0, 0],
                            [0, 0],
                            [0, 0]])
@@ -29,3 +30,16 @@ def test_daily_mean_integers():
     # Need to use Numpy testing functions to compare arrays
     npt.assert_array_equal(daily_mean(test_input), test_result)
 
+@pytest.mark.parametrize('data,expected_output', [
+    ([[[0, 1, 0], [0, 2, 0]]], [0, 0, 0]),
+    ([[[0, 2, 0]], [[0, 1, 0]]], [0, math.sqrt(0.25), 0]),
+    ([[[0, 1, 0], [0, 2, 0]], [[0, 1, 0], [0, 2, 0]]], [0, 0, 0])
+],
+ids=['Two patients in same file',
+     'Two patients in different files',
+     'Two identical patients in two different files'])
+def test_compute_standard_deviation_by_day(data, expected_output):
+    from inflammation.compute_data import compute_standard_deviation_by_day
+    result = compute_standard_deviation_by_day(data)
+    npt.assert_array_almost_equal(result, expected_output)
+    
